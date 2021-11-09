d442e1a932173bf019cb6effb9840a7c1c815e03
Nikola Lukovic <lukovic89@gmail.com>
2021-11-09 09:48:19 +0100

    reactPDF

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import jsPDF from "jspdf";
+import pdfMake from "pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import htmlToPdfmake from "html-to-pdfmake";
+
+class App extends React.Component {
+  printDocument() {
+    //const input = document.getElementById('divToPrint');
+
+    const doc = new jsPDF();
+
+    //get table html
+    const pdfTable = document.getElementById("divToPrint");
+    //html to pdf format
+    var html = htmlToPdfmake(pdfTable.innerHTML);
+
+    const documentDefinition = { content: html };
+    pdfMake.vfs = pdfFonts.pdfMake.vfs;
+    pdfMake.createPdf(documentDefinition).open();
+  }
+
+  render() {
+    return (
+      <div className="App container mt-5">
+        <div id="divToPrint">
+          <h2>therichpost.com</h2>
+
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Website</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Ajay</td>
+                <td>Malhotra</td>
+                <td>Therichpost.com</td>
+              </tr>
+              <tr>
+                <td>Ajay</td>
+                <td>Malhotra</td>
+                <td>Therichpost.com</td>
+              </tr>
+              <tr>
+                <td>Ajay</td>
+                <td>Malhotra</td>
+                <td>Therichpost.com</td>
+              </tr>
+              <tr>
+                <td>Ajay</td>
+                <td>Malhotra</td>
+                <td>Therichpost.com</td>
+              </tr>
+              <tr>
+                <td>Jassa</td>
+                <td>Malhotra</td>
+                <td>Therichpost.com</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <button class="btn btn-primary" onClick={this.printDocument}>
+          Export To PDF
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
