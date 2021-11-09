9395b3502a148b8f12d0484d12facee9f90ac247
Nikola Lukovic <lukovic89@gmail.com>
2021-11-09 18:42:28 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -6,69 +6,113 @@ import jsPDF from "jspdf";
 import pdfMake from "pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
 import htmlToPdfmake from "html-to-pdfmake";
+import { useEffect, useState } from "react";
 
+let firstname = "";
+let lastname = "";
+let woSubject = "";
+let woStatus = "";
+let firstnameA = "";
+let lastnameA = "";
+let woSubjectA = "";
+let woStatusW = "";
 class App extends React.Component {
-  printDocument() {
-    //const input = document.getElementById('divToPrint');
+  componentDidMount() {
+    console.log("POYY");
+    const queryString = ` data={!Print_data_set__c}&firstname={!User.firstName}&firstnameD={Alessandro}&lastname={!User.lastName}&lastnameA={Fiorentino}&subject={!Subject}&status={!Status}&subjectA={APP}&statusW={WIP}`;
 
-    const doc = new jsPDF();
+    const qsUrl = queryString.slice(1);
+
+    const mapQsUrl = qsUrl
+      .split("&")
+      .filter((el) => el)
+      .reduce(
+        (acc, el) =>
+          Object.assign(acc, {
+            [decodeURIComponent(el.split("=")[0].trim())]: decodeURIComponent(
+              el.split("=")[1].trim()
+            ),
+          }),
+        {}
+      );
+    firstname = mapQsUrl.firstname || "";
+    firstnameA = mapQsUrl.firstnameD || "";
+    lastnameA = mapQsUrl.lastnameA || "";
+    lastname = mapQsUrl.lastname || "";
+    woSubject = mapQsUrl.subject || "";
+    woStatus = mapQsUrl.status || "";
+    woSubjectA = mapQsUrl.subjectA || "";
+    woStatusW = mapQsUrl.statusW || "";
+  }
+
+  printDocument() {
+    //const doc = new jsPDF();
 
     //get table html
-    const pdfTable = document.getElementById("divToPrint");
+    //const pdfTable = document.getElementById("divToPrint");
     //html to pdf format
-    var html = htmlToPdfmake(pdfTable.innerHTML);
 
-    const documentDefinition = { content: html };
+    const html5 = `
+   
+    <h1 style="color:red;" >Field service</h1>
+		<div>
+			<table>
+
+				<tr>
+					<th>FirstName</th>
+					<th>LastName</th>
+				</tr>
+				<tr>
+					<td >${firstname}</td>
+					<td >${lastname}</td>
+				</tr>
+        <tr>
+        <td >${firstnameA}</td>
+        <td >${lastnameA}</td>
+      </tr>
+			</table>
+            <br/>
+            <table>
+				<tr>
+					<th>Work Order Subject</th>
+					<th>Work Order status</th>
+				  </tr>
+				  <tr>
+					<td >${woSubject}</td>
+					<td >${woStatus}</td>
+				  </tr>
+          <tr>
+					<td >${woSubjectA}</td>
+					<td >${woStatusW}</td>
+				  </tr>
+			</table>
+		</div>		
+      `;
+    var html = htmlToPdfmake(html5);
+
+    const documentDefinition = {
+      content: html,
+      styles: {
+        header: {
+          fontSize: 22,
+          bold: true,
+        },
+        anotherStyle: {
+          italics: true,
+        },
+      },
+    };
     pdfMake.vfs = pdfFonts.pdfMake.vfs;
     pdfMake.createPdf(documentDefinition).open();
   }
 
   render() {
     return (
-      <div className="App container mt-5">
-        <div id="divToPrint">
-          <h2>therichpost.com</h2>
+      <div>
+        <h2>HTML5 TO PDF APP</h2>
 
-          <table class="table table-bordered">
-            <thead>
-              <tr>
-                <th>Firstname</th>
-                <th>Lastname</th>
-                <th>Website</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Ajay</td>
-                <td>Malhotra</td>
-                <td>Therichpost.com</td>
-              </tr>
-              <tr>
-                <td>Ajay</td>
-                <td>Malhotra</td>
-                <td>Therichpost.com</td>
-              </tr>
-              <tr>
-                <td>Ajay</td>
-                <td>Malhotra</td>
-                <td>Therichpost.com</td>
-              </tr>
-              <tr>
-                <td>Ajay</td>
-                <td>Malhotra</td>
-                <td>Therichpost.com</td>
-              </tr>
-              <tr>
-                <td>Jassa</td>
-                <td>Malhotra</td>
-                <td>Therichpost.com</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-
-        <button class="btn btn-primary" onClick={this.printDocument}>
-          Export To PDF
+        <button className="btn btn-primary" onClick={this.printDocument}>
+          Export HTML5 To PDF
         </button>
       </div>
     );
